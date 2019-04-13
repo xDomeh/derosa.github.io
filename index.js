3ac0ea7e6b08768e28e024d2d604e473d28f8734
DXNC_HSR <35150923+xDomeh@users.noreply.github.com>
2019-04-13 23:47:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelectorAll(".btn")[0].addEventListener("click", function() {
+  var sound = new Audio("sounds/si.mp3");
+  sound.play();
+});
+
+document.querySelectorAll(".btn")[1].addEventListener("click", function() {
+  var sound = new Audio("sounds/no.mp3");
+  sound.play();
+});
+
+document.querySelectorAll(".btn")[2].addEventListener("click", function() {
+  var sound = new Audio("sounds/oba.mp3");
+  sound.play();
+});
+
+document.querySelectorAll(".btn")[3].addEventListener("click", function() {
+  var sound = new Audio("sounds/51.mp3");
+  sound.play();
+});
