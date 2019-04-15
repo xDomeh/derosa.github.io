be2abea403e7c8512d38f9d59ef4fd2a92a4786c
Dom <35150923+xDomeh@users.noreply.github.com>
2019-04-15 17:56:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
-document.querySelectorAll(".btn")[0].addEventListener("click", function() {
-  var sound = new Audio("sounds/si.mp3");
-  sound.play();
+/*document.querySelectorAll('.btn')[0].addEventListener('click', function() {
+	var sound = new Audio('sounds/si.mp3');
+	sound.play();
 });
 
-document.querySelectorAll(".btn")[1].addEventListener("click", function() {
-  var sound = new Audio("sounds/no.mp3");
-  sound.play();
+document.querySelectorAll('.btn')[1].addEventListener('click', function() {
+	var sound = new Audio('sounds/no.mp3');
+	sound.play();
 });
 
-document.querySelectorAll(".btn")[2].addEventListener("click", function() {
-  var sound = new Audio("sounds/oba.mp3");
-  sound.play();
+document.querySelectorAll('.btn')[2].addEventListener('click', function() {
+	var sound = new Audio('sounds/oba.mp3');
+	sound.play();
 });
 
-document.querySelectorAll(".btn")[3].addEventListener("click", function() {
-  var sound = new Audio("sounds/51.mp3");
-  sound.play();
+document.querySelectorAll('.btn')[3].addEventListener('click', function() {
+	var sound = new Audio('sounds/51.mp3');
+	sound.play();
 });
 
-document.querySelectorAll(".btn")[5].addEventListener("click", function() {
-  var sound = new Audio("sounds/dai.mp3");
-  sound.play();
+document.querySelectorAll('.btn')[5].addEventListener('click', function() {
+	var sound = new Audio('sounds/dai.mp3');
+	sound.play();
+});*/
+
+document.querySelectorAll('.btn')[0].addEventListener('click', function initAudio() {
+	var audio = new Audio('.sounds/si.mp3');
+	var self = this;
+	self.audio = audio;
+	var startAudio = function() {
+		self.audio.play();
+		document.removeEventListener('touchstart', self.startAudio, false);
+	};
+	self.startAudio = startAudio;
+
+	var pauseAudio = function() {
+		self.audio.pause();
+		self.audio.removeEventListener('play', self.pauseAudio, false);
+	};
+	self.pauseAudio = pauseAudio;
+
+	document.addEventListener('touchstart', self.startAudio, false);
+	self.audio.addEventListener('play', self.pauseAudio, false);
+});
+
+document.querySelectorAll('.btn')[1].addEventListener('click', function initAudio() {
+	var audio = new Audio('.sounds/si.mp3');
+	var self = this;
+	self.audio = audio;
+	var startAudio = function() {
+		self.audio.play();
+		document.removeEventListener('touchstart', self.startAudio, false);
+	};
+	self.startAudio = startAudio;
+
+	var pauseAudio = function() {
+		self.audio.pause();
+		self.audio.removeEventListener('play', self.pauseAudio, false);
+	};
+	self.pauseAudio = pauseAudio;
+
+	document.addEventListener('touchstart', self.startAudio, false);
+	self.audio.addEventListener('play', self.pauseAudio, false);
 });
