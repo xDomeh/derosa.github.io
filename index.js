d0a6a08829b6840e54e7398be9408210943dce60
Dom <35150923+xDomeh@users.noreply.github.com>
2019-04-16 16:46:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-document.querySelectorAll('.btn')[0].addEventListener('touchstart', function() {
+document.querySelectorAll('.btn')[0].addEventListener('click', function() {
 	var sound = new Audio('sounds/si.mp3');
 	sound.play();
 });
 
-document.querySelectorAll('.btn')[1].addEventListener('touchstart', function() {
+document.querySelectorAll('.btn')[1].addEventListener('click', function() {
 	var sound = new Audio('sounds/no.mp3');
 	sound.play();
 });
 
-document.querySelectorAll('.btn')[2].addEventListener('touchstart', function() {
+document.querySelectorAll('.btn')[2].addEventListener('click', function() {
 	var sound = new Audio('sounds/oba.mp3');
 	sound.play();
 });
 
-document.querySelectorAll('.btn')[3].addEventListener('touchstart', function() {
+document.querySelectorAll('.btn')[3].addEventListener('click', function() {
 	var sound = new Audio('sounds/51.mp3');
 	sound.play();
 });
 
-document.querySelectorAll('.btn')[4].addEventListener('touchstart', function() {
+document.querySelectorAll('.btn')[4].addEventListener('click', function() {
 	var sound = new Audio('sounds/cazzo.mp3');
 	sound.play();
 });
 
-document.querySelectorAll('.btn')[5].addEventListener('touchstart', function() {
+document.querySelectorAll('.btn')[5].addEventListener('click', function() {
 	var sound = new Audio('sounds/dai.mp3');
 	sound.play();
 });
